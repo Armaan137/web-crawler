b193d84641843450bd3f509e1b618554cc43da1d
Armaan137 <a.khabra5@gmail.com>
2025-08-10 07:30:04 +0000

    add the http part to main to test it

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "main.hpp"
+#include "http.hpp"
 
 int main() {
-    std::cout << "Crawler Test.\n";
+    std::cout << "Crawler Test." << '\n';
+    fetch_url("test.com");
 
     return 0;
 }
